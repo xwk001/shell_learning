20fcd1e4241bde586d175c22a0116ecf80bed44e
xwk001 <834299836@qq.com>
2022-03-27 08:48:20 -0700

    1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-<>/p</>
+<html>
+  <head>
+    <link rel="stylesheet" href="index.css" />
+  </head>
+  <body>
+    <nav id="nav">
+      <ul>
+        <li></li>
+      </ul>
+    </nav>
+  </body>
+</html>
