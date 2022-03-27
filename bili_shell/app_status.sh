34b24710762727c27ccdc76bd5f7754d15c206a9
xwk001 <834299836@qq.com>
2022-03-27 08:40:18 -0700

    new modyfied

--- bili_shell/app_status.sh
+++ bili_shell/app_status.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+function get_group_by_process_name
+{
+    for gn in `function get_all_group`;do
+        for pn in `function get_all_process_by_group $gn`;do
+            if [ "$gn" == "$1" ];lhen
+               echo "$gn"   
+            fi
+        done
+    done
+}
 
 if [ $# -gt 0 ];then
     if [ $1 == -g ];then
